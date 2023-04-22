5e81af786df631fa260014c945ead8991566a6a7
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-04-22 15:43:15 +0300

    Initial commit
    Header and Home Page Created

---
+++ pos-application/client/src/App.jsx
@@ -0,0 +1,23 @@
+import Categories from "./components/categories/Categories";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div>Products</div>
+        </div>
+        <div className="cartTotals">
+          <div>Cart Totals</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
